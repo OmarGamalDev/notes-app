5f27e3096e067c882c54a189e4001fd58b4d3eb2
Omar Gamal <omarthefirst6206@gmail.com>
2025-09-18 01:54:25 +0300

    feat: add SimpleBlocObserver for Bloc state management and debugging

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/core/constants/routes.dart';
 import 'package:notes_app/features/notes/data/models/note_model.dart';
 import 'package:notes_app/features/splash/presentation/views/splash_view.dart';
+import 'package:notes_app/simple_bloc_observer.dart';
 
 void main()async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
   await Hive.openBox('notes_box');
   Hive.registerAdapter(NoteModelAdapter());
